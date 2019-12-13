3db6a57e9fc4b4dc6a7c4976f1c9bfcae9cd2a80
Tomas Ortiz <TomasOrtiz@protonmail.com>
2019-12-13 09:16:36 -0800

    setting up infrasructure wip

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,13 +1,35 @@
 import React from 'react';
 import './App.css';
+import axios from "axios";
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
 
-const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
+// Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
   // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+
+const App = () => {
+
+const [chars, setChars] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(
+        ("https://swapi.co/api/people/")
+      
+      .then(res => {
+        console.log("Fetched!", res);
+        setChars(res.data);
+      })
+      .catch(err => {
+        console.log("Error: The data was not returned!", err);
+      });
+  }, []);
+
+  
 
   return (
     <div className="App">
@@ -15,5 +37,6 @@ const App = () => {
     </div>
   );
 }
+
 
 export default App;
---
+++ starwars/src/starWarsCard.js
@@ -0,0 +1,29 @@
+import React from "react";
+import styled from 'styled-components';
+
+export const SwCard = styled.div `
+background-color: grey;
+text-align: left;
+width: 10%;
+`
+
+export const SwContainer = styled.div`
+display: flex;
+justify-content: center;
+flex-wrap: nowrap;
+`
+
+export const SwHeader = styled.h1`
+font-size: 30px;
+color: aqua;
+text-align: center;
+`
+
+export const SwH4 = styled.h4`
+color: purple;
+`
+
+// export default function SwCharacters({props}) {
+
+// }
+
